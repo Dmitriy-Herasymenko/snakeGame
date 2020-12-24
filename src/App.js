76c4d8a53540568f85ee00b5939d8f00bb6ea848
embellisher <55753239+embellisher@users.noreply.github.com>
2020-12-24 11:30:55 +0200

    add move snake

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
+import Snake from "./Snake";
+import Food from "./Food";
 
 function App() {
+
+const getRandomCoordinates = () => {
+  let min = 1;
+  let max = 98;
+  let x = Math.floor((Math.random()*(max-min+1)/2)*2);
+  let y = Math.floor((Math.random()*(max-min+1)/2)*2);
+  return [x,y]
+};
+
+
+const [state, setState] = useState({
+  direction: 'RIGHT',
+  food: getRandomCoordinates(),
+  speed: 200,
+  snakeDots: [
+    [0,0],
+    [2,0]
+  ]
+});
+
+useEffect( () => {
+ document.onkeydown = onKeyDown
+}, [state.direction]);
+
+useEffect( () => {
+  setInterval( () => {
+    moveSnake();
+ }, state.speed)
+}, [state.snakeDots]);
+
+const setDirection = key => {
+   setState(prev => {
+     return {
+       ...prev,
+       direction: key,
+     }
+   });
+ };
+
+  const setSnakeDots = dots => {
+
+    setState(prev => {
+      return {
+        ...prev,
+        snakeDots: dots,
+      }
+    });
+  };
+
+  const onKeyDown = e => {
+    e = e || window.event;
+    switch (e.keyCode) {
+      case 38:
+        setDirection('UP');
+        break;
+    }
+    switch (e.keyCode) {
+      case 40:
+        setDirection('DOWN');
+        break;
+    }
+    switch (e.keyCode) {
+      case 37:
+        setDirection('LEFT');
+        break;
+    }
+    switch (e.keyCode) {
+      case 39:
+        setDirection('RIGHT');
+        break;
+    }
+  };
+
+  const moveSnake = () => {
+    let dots = [...state.snakeDots];
+    let head = dots[dots.length - 1];
+    console.log("head", head)
+    switch (state.direction) {
+      case 'RIGHT':
+        head = [head[0] + 2, head[1]];
+        break;
+      case 'LEFT':
+        head = [head[0] - 2, head[1]];
+        break;
+      case 'DOWN':
+        head = [head[0], head[1] + 2];
+        break;
+      case 'UP':
+        head = [head[0], head[1] - 2];
+        break;
+    }
+    dots = [...dots, head];
+    setSnakeDots(dots);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game-arena">
+      <Snake snakeDots={state.snakeDots}/>
+      <Food dot={state.food}/>
+
     </div>
   );
 }
---
+++ src/Food.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Food (props) {
+    const style = {
+        left: `${props.dot[0]}%`,
+        top: `${props.dot[1]}%`
+    }
+
+    return <div className='food' style={style}/>
+}
